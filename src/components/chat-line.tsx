e41008cc55402383772ea593c9720e8cd7600ff7
SideeID <sandipratama164026@gmail.com>
2024-09-28 20:19:22 +0700

    Add ChatLine component for displaying chat messages

---
+++ src/components/chat-line.tsx
@@ -0,0 +1,72 @@
+import Balancer from 'react-wrap-balancer';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
+import { Message } from 'ai/react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import { formattedText } from '@/lib/utils';
+
+interface ChatLineProps extends Partial<Message> {}
+
+export function ChatLine({ role = 'assistant', content }: ChatLineProps) {
+  if (!content) {
+    return null;
+  }
+
+  return (
+    <div>
+      <Card className='mb-2'>
+        <CardHeader>
+          <CardTitle
+            className={
+              role !== 'assistant'
+                ? 'text-amber-500 dark:text-amber-200'
+                : 'text-blue-500 dark:text-blue-200'
+            }
+          >
+            {role === 'assistant' ? 'JEMPOL' : 'Anda'}
+          </CardTitle>
+        </CardHeader>
+        <CardContent className='text-sm break-words whitespace-normal overflow-auto'>
+          <Balancer>
+            <ReactMarkdown remarkPlugins={[remarkGfm]} linkTarget='_blank'>
+              {content}
+            </ReactMarkdown>
+          </Balancer>
+        </CardContent>
+        {/* <CardFooter>
+          <CardDescription className="w-full">
+            {sources && sources.length ? (
+              <Accordion type="single" collapsible className="w-full">
+                {sources.map((source, index) => (
+                  <AccordionItem value={`source-${index}`} key={index}>
+                    <AccordionTrigger>{`Source ${index + 1}`}</AccordionTrigger>
+                    <AccordionContent>
+                      <ReactMarkdown linkTarget="_blank">
+                        {formattedText(source)}
+                      </ReactMarkdown>
+                    </AccordionContent>
+                  </AccordionItem>
+                ))}
+              </Accordion>
+            ) : (
+              <></>
+            )}
+          </CardDescription>
+        </CardFooter> */}
+      </Card>
+    </div>
+  );
+}
