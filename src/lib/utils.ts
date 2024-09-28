5d23b623ded7a467a8c95a9ac9a7512c9d79710a
SideeID <sandipratama164026@gmail.com>
2024-09-28 20:20:38 +0700

    Add utils module for common utility functions

---
+++ src/lib/utils.ts
@@ -0,0 +1,68 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { Message as StreamMessage } from 'ai-stream-experimental/react';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export function scrollToBottom(containerRef: React.RefObject<HTMLElement>) {
+  if (containerRef.current) {
+    const lastMessage = containerRef.current.lastElementChild;
+    if (lastMessage) {
+      const scrollOptions: ScrollIntoViewOptions = {
+        behavior: "smooth",
+        block: "end",
+      };
+      lastMessage.scrollIntoView(scrollOptions);
+    }
+  }
+}
+
+// Reference:
+// github.com/hwchase17/langchainjs/blob/357d6fccfc78f1332b54d2302d92e12f0861c12c/examples/src/guides/expression_language/cookbook_conversational_retrieval.ts#L61
+export const formatChatHistory = (chatHistory: [string, string][]) => {
+  const formattedDialogueTurns = chatHistory.map(
+    (dialogueTurn) => `Human: ${dialogueTurn[0]}\nAssistant: ${dialogueTurn[1]}`
+  );
+
+  return formattedDialogueTurns.join("\n");
+};
+
+export function formattedText(inputText: string) {
+  return inputText
+    .replace(/\n+/g, ' ') // Mengganti beberapa baris baru yang berurutan dengan satu spasi
+    .replace(/(\w) - (\w)/g, '$1$2') // Menggabungkan kata-kata yang diberi tanda hubung menjadi satu
+    .replace(/\s+/g, ' '); // Mengganti beberapa spasi berurutan dengan satu spasi
+}
+
+// Default UI Message
+export const initialMessages: StreamMessage[] = [
+  {
+    role: 'assistant',
+    id: '0',
+    content: `
+      Selamat datang di layanan otomatis PINTU (Pusat Informasi dan Pelayanan Terpadu) Politeknik Negeri Jember! 🎓✨
+
+      Saya adalah asisten virtual Anda, siap membantu menjawab segala pertanyaan dan kebutuhan informasi Anda dengan cepat dan akurat.
+      Apakah ada yang bisa saya bantu hari ini? 😊
+
+      Apa yang ingin Anda tanyakan?
+
+      🔍 Ingin tahu informasi terbaru seputar kampus?
+      📚 Bingung dengan urusan akademik dan kemahasiswaan?
+      📞 Mencari kontak penting di lingkungan kampus?
+      ❓ Atau butuh bantuan lainnya?
+
+      Saya di sini untuk membantu Anda! Mari kita mulai petualangan informasi Anda bersama! 🌟
+    `,
+  },
+];
+
+interface Data {
+  sources: string[];
+}
