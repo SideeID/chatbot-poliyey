d0937d736ea499f087b418598cc6074e1c878ce7
SideeID <sandipratama164026@gmail.com>
2024-09-28 20:20:28 +0700

    Add Pinecone client module for managing Pinecone indexes

---
+++ src/lib/pinecone-client.ts
@@ -0,0 +1,67 @@
+import { Pinecone } from '@pinecone-database/pinecone';
+import { env } from './config';
+import { delay } from './utils';
+
+let pineconeClientInstance: Pinecone | null = null;
+
+// Create pineconeIndex if it doesn't exist
+async function createIndex(client: Pinecone, indexName: string) {
+  try {
+    await client.createIndex({
+      name: indexName,
+      dimension: 768,
+      metric: 'cosine',
+      spec: {
+        serverless: {
+          cloud: 'aws',
+          region: 'us-east-1',
+        },
+      },
+      deletionProtection: 'disabled',
+    });
+    console.log(
+      `Waiting for ${env.INDEX_INIT_TIMEOUT} seconds for index initialization to complete...`
+    );
+    await delay(env.INDEX_INIT_TIMEOUT);
+    console.log('Index created !!');
+  } catch (error) {
+    console.error('error ', error);
+    throw new Error('Index creation failed');
+  }
+}
+
+// Initialize index and ready to be accessed.
+async function initPineconeClient() {
+  try {
+    const pineconeClient = new Pinecone({
+      apiKey: env.PINECONE_API_KEY,
+    });
+    const indexName = env.PINECONE_INDEX_NAME;
+
+    const existingIndexes = await pineconeClient.listIndexes();
+
+    // Check if indexes property exists and is an array
+    if (existingIndexes && Array.isArray(existingIndexes.indexes)) {
+      if (!existingIndexes.indexes.map(index => index.name).includes(indexName)) {
+        await createIndex(pineconeClient, indexName);
+      } else {
+        console.log('Index already exists');
+      }
+    } else {
+      console.warn('No indexes found or indexes property is not an array.');
+    }
+
+    return pineconeClient;
+  } catch (error) {
+    console.error('error', error);
+    throw new Error('Failed to initialize Pinecone Client');
+  }
+}
+
+export async function getPineconeClient() {
+  if (!pineconeClientInstance) {
+    pineconeClientInstance = await initPineconeClient();
+  }
+
+  return pineconeClientInstance;
+}
