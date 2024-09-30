26688b92da239cf09098c6f30decd539247b4504
SideeID <sandipratama164026@gmail.com>
2024-09-30 17:36:45 +0700

    Refactor pdf-loader.ts: Update document loading logic

--- src/lib/pdf-loader.ts
+++ src/lib/pdf-loader.ts
@@ -1,11 +1,19 @@
 import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
+import { TextLoader } from 'langchain/document_loaders/fs/text';
+import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import { env } from './config';
 
 export async function getChunkedDocsFromPDF() {
   try {
-    const loader = new PDFLoader(env.PDF_PATH);
+    // const loader = new PDFLoader(env.PDF_PATH);
+    const loader = new DirectoryLoader('./docs', {
+      '.pdf': (path) => new PDFLoader(path),
+      '.txt': (path) => new TextLoader(path),
+    });
+
     const docs = await loader.load();
+    // console.log(docs[0].metadata);
 
     // https://www.pinecone.io/learn/chunking-strategies/
     const textSplitter = new RecursiveCharacterTextSplitter({
