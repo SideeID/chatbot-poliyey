e85e7dddf3e150e11a174747801aa3f2e7993ebb
SideeID <sandipratama164026@gmail.com>
2024-10-01 15:59:19 +0700

    Refactor test.txt: Remove unused code and add commit message guidelines

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className='relative container flex min-h-screen flex-col'>
-      <div className=' p-4 flex h-14 items-center justify-between supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur'>
+      <div className='p-4 flex h-14 items-center justify-between supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full bg-background/95 backdrop-blur'>
         <Image
           src='/logoPoltek.webp'
           alt='JEMPOL'
@@ -13,9 +13,6 @@ export default function Home() {
           height={125}
           loading='lazy'
         />
-        {/* <span className='font-bold'>
-          Jaringan Edukasi dan Informasi Layanan Mandiri POLIJE
-        </span> */}
         <DarkModeToggle />
       </div>
       <div className='flex flex-1 py-4'>
@@ -23,7 +20,7 @@ export default function Home() {
           <Chat />
         </div>
       </div>
-      <footer className='py-4 border-t text-center text-sm bg-background copyright'>
+      <footer className='py-4 text-center text-sm bg-background copyright'>
         <a
           href='https://github.com/SideeID'
           target='_blank'
