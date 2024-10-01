3a1ac552e0c4eee5e6e47593a07db02abbb1700d
SideeID <sandipratama164026@gmail.com>
2024-10-01 12:41:38 +0700

    Refactor pinecone-prepare-docs.ts: Update import statement for file loader

--- src/scripts/pinecone-prepare-docs.ts
+++ src/scripts/pinecone-prepare-docs.ts
@@ -1,4 +1,4 @@
-import { getChunkedDocsFromPDF } from "@/lib/pdf-loader";
+import { getChunkedDocsFromPDF } from "@/lib/file-loader";
 import { embedAndStoreDocs } from "@/lib/vector-store";
 import { getPineconeClient } from "@/lib/pinecone-client";
 
