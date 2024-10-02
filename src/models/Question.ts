467acc08216eddb7ca680eedb352ad0243165d64
SideeID <sandipratama164026@gmail.com>
2024-10-02 22:12:27 +0700

    Add Question model for storing questions in the database

---
+++ src/models/Question.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const QuestionSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: [true, 'Please provide the question content'],
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+  isAnswered: {
+    type: Boolean,
+    default: false,
+  },
+  answer: {
+    type: String,
+    default: '',
+  },
+});
+
+export default mongoose.models.Question ||
+  mongoose.model('Question', QuestionSchema);
