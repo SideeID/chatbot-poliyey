1a1292ec288881a83b25ba6a6c5dc588eb114c61
SideeID <sandipratama164026@gmail.com>
2024-11-30 19:58:08 +0700

    Refactor llm.ts: Replace OpenAI model implementation with Google Generative AI

--- src/lib/llm.ts
+++ src/lib/llm.ts
@@ -1,18 +1,3 @@
-// import { ChatOpenAI } from "langchain/chat_models/openai";
-
-// export const streamingModel = new ChatOpenAI({
-//   modelName: "gpt-3.5-turbo",
-//   streaming: true,
-//   verbose: true,
-//   temperature: 0,
-// });
-
-// export const nonStreamingModel = new ChatOpenAI({
-//   modelName: "gpt-3.5-turbo",
-//   verbose: true,
-//   temperature: 0,
-// });
-
 import { ChatGoogleGenerativeAI } from '@langchain/google-genai';
 
 export const streamingModel = new ChatGoogleGenerativeAI({
