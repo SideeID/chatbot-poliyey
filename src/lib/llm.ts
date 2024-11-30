0c0a1b67930edb003274d862ce8f3e45b0affe9d
SideeID <sandipratama164026@gmail.com>
2024-11-30 18:38:11 +0700

    change model llm

--- src/lib/llm.ts
+++ src/lib/llm.ts
@@ -16,7 +16,7 @@
 import { ChatGoogleGenerativeAI } from '@langchain/google-genai';
 
 export const streamingModel = new ChatGoogleGenerativeAI({
-  model: 'gemini-1.5-flash',
+  model: 'gemini-1.5-flash-8b',
   temperature: 0,
   maxRetries: 2,
   streaming: true,
@@ -24,7 +24,7 @@ export const streamingModel = new ChatGoogleGenerativeAI({
 });
 
 export const nonStreamingModel = new ChatGoogleGenerativeAI({
-  model: 'gemini-1.5-flash',
+  model: 'gemini-1.5-flash-8b',
   temperature: 0,
   maxRetries: 2,
   verbose: true,
