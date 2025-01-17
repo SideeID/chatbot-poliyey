62d5f61007367460dd6ac6c8237118585abda302
SideeID <sandipratama164026@gmail.com>
2025-01-17 20:47:03 +0700

    Remove commented-out sitemap entries for cleaner code

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,12 +6,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: 'https://jempol.sideid.tech',
       lastModified: new Date(),
     },
-    // {
-    //   url: 'https://jempol.sideid.tech/tentang',
-    //   lastModified: new Date(),
-    //   changeFrequency: 'weekly',
-    //   priority: 0.8,
-    // },
-    // {
   ];
 }
