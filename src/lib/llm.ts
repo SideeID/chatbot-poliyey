12f8664ad02a0c821c4dbd8243c3d8e789eee52a
SideeID <sandipratama164026@gmail.com>
2025-01-30 20:24:59 +0700

    Replace ChatGoogleGenerativeAI with ChatGroq for streaming model and update model parameters

--- src/lib/llm.ts
+++ src/lib/llm.ts
@@ -1,7 +1,8 @@
 import { ChatGoogleGenerativeAI } from '@langchain/google-genai';
+import { ChatGroq } from '@langchain/groq';
 
-export const streamingModel = new ChatGoogleGenerativeAI({
-  model: 'gemini-1.5-flash-8b',
+export const streamingModel = new ChatGroq({
+  model: 'deepseek-r1-distill-llama-70b',
   temperature: 0.2,
   maxRetries: 2,
   streaming: true,
@@ -9,8 +10,24 @@ export const streamingModel = new ChatGoogleGenerativeAI({
 });
 
 export const nonStreamingModel = new ChatGoogleGenerativeAI({
-  model: 'gemini-1.5-flash-8b',
+  model: 'gemini-1.5-flash',
   temperature: 0.2,
   maxRetries: 2,
   verbose: true,
 });
+
+
+// export const streamingModel = new ChatGoogleGenerativeAI({
+//   model: 'gemini-1.5-flash',
+//   temperature: 0.2,
+//   maxRetries: 2,
+//   streaming: true,
+//   verbose: true,
+// });
+
+// export const nonStreamingModel = new ChatGroq({
+//   model: 'deepseek-r1-distill-llama-70b',
+//   temperature: 0.2,
+//   maxRetries: 2,
+//   verbose: true,
+// });
