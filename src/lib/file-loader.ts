fcd24a070ede657dff633de5b8fff11b47ac1c55
SideeID <sandipratama164026@gmail.com>
2025-02-14 10:36:37 +0700

    Add file path comment to file-loader.ts for clarity

--- src/lib/file-loader.ts
+++ src/lib/file-loader.ts
@@ -1,3 +1,4 @@
+// src\lib\file-loader.ts
 import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
 import { TextLoader } from 'langchain/document_loaders/fs/text';
 import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
